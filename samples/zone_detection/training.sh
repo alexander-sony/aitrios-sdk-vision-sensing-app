6f9ce98e9c32a8da7d9cca5f94139e084eb7e3a0
Alexander Jaworowski <alexander.jaworowski@sony.com>
2023-09-26 16:01:45 +0200

    Fix

--- samples/zone_detection/training.sh
+++ samples/zone_detection/training.sh
@@ -8,7 +8,7 @@ NUM_TRAIN_STEPS=300
 PIPELINE_CONFIG_PATH=$TRAINING_DOCKER_VOLUME_DIR/$MODELS_DIR/pipeline.config
 MODEL_DIR=$TRAINING_DOCKER_VOLUME_DIR/$MODELS_DIR/out/train
 SAMPLE_1_OF_N_EVAL_EXAMPLES=100
-docker run --rm -t -v $pwd:$TRAINING_DOCKER_VOLUME_DIR \
+docker run --rm -t -v $PWD:$TRAINING_DOCKER_VOLUME_DIR \
     --network host $TRAINING_DOCKER_IMAGE_NAME \
     python /tensorflow/models/research/object_detection/model_main.py \
     --pipeline_config_path=$PIPELINE_CONFIG_PATH \
